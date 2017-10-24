6b4cd87784a5b1843a1590c62c55fc0be1f5fbca
Balázs Koltai <koltai.b@gmail.com>
2017-10-24 13:26:46 +0200

    Added room switching class, separated grid generator and player tracker

---
+++ Escape Room/Assets/Scripts/PlayerTracker.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+
+    public Transform trackedObject;
+    LocDirID locationDirection = new LocDirID();
+    Coroutine lastCoroutine = null;
+
+
+    void Start()
+    {
+        if (lastCoroutine == null)
+        {
+            lastCoroutine = StartCoroutine(CheckLocationAndDirection());
+        }
+    }
+
+    // void Update()
+    // {
+    //     if (locationDirection.tile == 28 && locationDirection.dir == 100) //Test conditions, we'll remove this once proper condition handling is done
+    //     {
+    //         StopCoroutine(lastCoroutine);
+    //     }
+    // }
+
+    void GetTileID()
+    {
+        Ray ray = new Ray(trackedObject.transform.position, Vector3.down);
+        RaycastHit hitInfo;
+        int tileMask = LayerMask.GetMask("Tile");
+
+        if (Physics.Raycast(ray, out hitInfo, 100, tileMask)) //Casts a ray down that only takes the Tile layer into consideration, puts the ray information in hitInfo
+        {
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red); //Draws a red line if you hit a tile
+            //Debug.Log(hitInfo.collider.GetComponent<Tile>().GetID()); //Writes the tile ID in the console (0 to 47 in this case)
+            locationDirection.tile = hitInfo.collider.GetComponent<Tile>().GetID();
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green); //Draws a green line if it hits anything else
+			locationDirection.tile = -1;
+        }
+    }
+
+    void LookingAtTile()
+    {
+        Ray ray = new Ray(trackedObject.transform.position, trackedObject.transform.forward);
+        RaycastHit hitInfo;
+        int tileMask = LayerMask.GetMask("DirectionTile");
+
+        if (Physics.Raycast(ray, out hitInfo, 100, tileMask))
+        {
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+            locationDirection.dir = hitInfo.collider.GetComponent<Tile>().GetID(); //Maybe make a separate script for wall tiles
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+            locationDirection.dir = -1;
+        }
+
+    }
+
+    public IEnumerator CheckLocationAndDirection() //checking ID and look direction is now a coroutine, we can change the update time in the yield return
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0f);
+            GetTileID();
+            LookingAtTile();
+        }
+    }
+
+    public LocDirID GetLocationAndDirection()
+    {
+		Debug.Log("aasd" + locationDirection.dir);
+        return locationDirection;
+    }
+}
+
+[System.Serializable]
+public struct LocDirID
+{
+    public int tile, dir; //tile and direction IDs stored
+
+    public LocDirID(int p1, int p2)
+    {
+        tile = p1;
+        dir = p2;
+    }
+}
---
+++ Escape Room/Assets/Scripts/RoomSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSwitcher : MonoBehaviour
+{
+
+    public PlayerTracker tracker;
+    LocDirID locationDirection = new LocDirID();
+
+	public GameObject[] Room;
+    public LocDirID[] switchCondition;
+    int currentRoom = 0; //could make public if we want to start in different rooms I guess.
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        locationDirection = tracker.GetLocationAndDirection();
+        Debug.Log(locationDirection.tile.ToString() + " , " + locationDirection.dir.ToString());
+
+        //add a condition for max number of rooms to avoid array index out of bounds error
+        if (CheckCondition(switchCondition[currentRoom]))
+        {
+            SwitchRoom();
+            currentRoom++;
+        }
+    }
+
+    void SwitchRoom()
+    {
+        Room[currentRoom].SetActive(false);
+		Room[currentRoom+1].SetActive(true);
+		//fix array overflow here too
+    }
+
+    bool CheckCondition(LocDirID condition)
+    {
+        return (locationDirection.tile == condition.tile && locationDirection.dir == condition.dir);
+    }
+}
