4973af1124050629732e0356ade342c7bc6915a8
Balázs Koltai <koltai.b@gmail.com>
2017-11-22 13:38:13 +0100

    Updated project to 2017.2, added BezierCurve class and tested it with spotlights looking at their respective curves as the sphere rotates.

---
+++ Escape Room/Assets/Scripts/BezierCurve.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, RequireComponent(typeof(LineRenderer))]
+public class BezierCurve : MonoBehaviour
+{
+
+    public Transform startPoint, middlePoint, endPoint, target;
+    public int numberOfSteps;
+    [Range(0, 360)] //steps capped at 360, a full rotation
+    public int currentPos;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //this is to check if GetPoint works, it does. The target gameobject is not necessary, we'll use the LookAt method for a spotlight in the sphere
+        target.position = GetPoint((float)currentPos);
+
+
+        //This here would be for drawing the bezier curve in the inspector, but I couldn't get it work yet
+        /*Vector3 position;
+        for (int i = 0; i < numberOfSteps; i++)
+        {
+            position = GetPoint(i / (float)numberOfSteps - 1f);
+        }*/
+    }
+
+    public Vector3 GetPoint(float pos) //here we can give the lightsphere's rotation to the bezier curve. neat.
+    {
+        float t = pos / (numberOfSteps - 1f);
+
+        Vector3 p0 = startPoint.position;
+        Vector3 p1 = middlePoint.position;
+        Vector3 p2 = endPoint.position;
+
+        return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+    }
+
+}
--- Escape Room/Assets/Scripts/LightSphere.cs
+++ Escape Room/Assets/Scripts/LightSphere.cs
@@ -7,46 +7,73 @@ using UnityEngine;
 public class LightSphere : MonoBehaviour
 {
 
-    public float wiggleRoom; //So the positions don't have to be extremely precise.
+    public BezierCurve[] curves;
+    public Transform sphere;
+    public GameObject[] lights;
+    public float wiggleRoom; //So the positions don't have to be extremely precise. wiggleRoom has to be at least 1, due to how float values are stored and compared
     [Range(0, 2)]
     public int currentLight; //It shows which light color we need to check for, also used to activate light visuals. 0-2 for the 3 colors so we can use them as array indexes.
     public float[] conditions; //positions where the location is correct, index will be the currentlight
     public float[] savedRotations; //we will store the rotation of the sphere here when we change colors
 
+    void Start()
+    {
+        lights[0].SetActive(true);
+        lights[1].SetActive(false);
+        lights[2].SetActive(false);
+    }
 
     void Update()
     {
-		if(Input.GetKeyDown("right")){
-			transform.Rotate(0,10,0);
-		}
+        //keyboard controls
+        if (Input.GetKeyDown("right"))
+        {
+            sphere.transform.Rotate(0, 10, 0);
+        }
 
-		if(Input.GetKeyDown("left")){
-			transform.Rotate(0,-10,0);
-		}
+        if (Input.GetKeyDown("left"))
+        {
+            sphere.transform.Rotate(0, -10, 0);
+        }
 
         if (Input.GetKeyDown("space"))
         {
-			Debug.Log(transform.eulerAngles.y);
+            Debug.Log(sphere.transform.eulerAngles.y);
             Debug.Log(CheckEveryCondition());
         }
 
-		if(Input.GetKeyDown("r")){
-			ChangeLight(0);
-		}
+        if (Input.GetKeyDown("r"))
+        {
+            ChangeLight(0);
+            lights[0].SetActive(true);
+            lights[1].SetActive(false);
+            lights[2].SetActive(false);
+        }
 
-		if(Input.GetKeyDown("g")){
-			ChangeLight(1);
-		}
+        if (Input.GetKeyDown("g"))
+        {
+            ChangeLight(1);
+            lights[0].SetActive(false);
+            lights[1].SetActive(true);
+            lights[2].SetActive(false);
+        }
 
-		if(Input.GetKeyDown("b")){
-			ChangeLight(2);
-		}
+        if (Input.GetKeyDown("b"))
+        {
+            ChangeLight(2);
+            lights[0].SetActive(false);
+            lights[1].SetActive(false);
+            lights[2].SetActive(true);
+        }
+
+        //The current light followes a Bezier curve. If we want we can modify it to a spline later so the transition at the 0 degree mark is smoother
+        lights[currentLight].transform.LookAt(curves[currentLight].GetPoint(sphere.transform.eulerAngles.y));
     }
 
     //c will be replaced with currentLight or all light indexes, so we can check conditions individually or all at the same time
     bool CheckCondition() //This could run into an error when the condition is near (closer than "wiggleRoom") 0 degrees, we should make sure this won't happen
     {
-        if (transform.eulerAngles.y == conditions[currentLight] || transform.eulerAngles.y >= conditions[currentLight] - wiggleRoom && transform.eulerAngles.y <= conditions[currentLight] + wiggleRoom)
+        if (sphere.transform.eulerAngles.y == conditions[currentLight] || sphere.transform.eulerAngles.y >= conditions[currentLight] - wiggleRoom && sphere.transform.eulerAngles.y <= conditions[currentLight] + wiggleRoom)
         {
             return true;
         }
@@ -75,13 +102,13 @@ public class LightSphere : MonoBehaviour
                 return false;
             }
         }
-		return true;
+        return true;
     }
 
     void ChangeLight(int light)
     {
-        savedRotations[currentLight] = transform.eulerAngles.y;
+        savedRotations[currentLight] = sphere.transform.eulerAngles.y;
         currentLight = light;
-		transform.rotation = Quaternion.Euler(0,savedRotations[light],0);
+        sphere.transform.rotation = Quaternion.Euler(0, savedRotations[light], 0);
     }
 }
