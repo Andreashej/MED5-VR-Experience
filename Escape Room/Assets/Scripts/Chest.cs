7f74627a09ae4519a1ac146bac32eb0b5f487bc6
Balázs Koltai <koltai.b@gmail.com>
2017-11-29 14:32:10 +0100

    Made flashlight prefab, chest prefab, added chest locking script, to be tested. Fixed corridor width.

---
+++ Escape Room/Assets/Scripts/Chest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public GameObject lid;
+    public Transform tracker;
+    public bool isTrackerVersion, isLocked;
+
+    void Start()
+    {
+		isLocked = true;
+        lid.GetComponent<Valve.VR.InteractionSystem.CircularDrive>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (isTrackerVersion)
+        {
+            if (!isLocked)
+            {
+                //I will put the rotation of the racker here
+            }
+        }
+    }
+
+    public void UnlockChest()
+    {
+        isLocked = false;
+        lid.GetComponent<Valve.VR.InteractionSystem.CircularDrive>().enabled = true;
+    }
+}
--- Escape Room/Assets/Scripts/LightRevealSource.cs
+++ Escape Room/Assets/Scripts/LightRevealSource.cs
@@ -3,48 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class LightRevealSource : MonoBehaviour {
+public class LightRevealSource : MonoBehaviour
+{
 
     public Material reveal;
-    public Light light;
+    public Light lightSource;
 
-    public Texture red;
-    public Texture blue;
-    public Texture yellow;
+    public Texture redTexture;
+    public Texture blueTexture;
+    public Texture greenTexture;
+    void Update()
+    {
 
-
-    // Use this for initialization
-    void Start () {
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
-        if(Input.GetKey("r"))
+        if (Input.GetKey("r"))
         {
-            light.color = Color.red;
-            reveal.SetTexture("_MainTex", red);
-			reveal.SetColor("_Color", Color.red);
-        } else if (Input.GetKey("b"))
-        {
-            light.color = Color.blue;
-            reveal.SetTexture("_MainTex", blue);
-			reveal.SetColor("_Color", Color.blue);
-        } else if (Input.GetKey("y"))
-        {
-            light.color = Color.yellow;
-            reveal.SetTexture("_MainTex", yellow);
-			reveal.SetColor("_Color", Color.yellow);
+            lightSource.color = Color.red;
+            reveal.SetTexture("_MainTex", redTexture);
+            reveal.SetColor("_Color", Color.red);
         }
-        else if(Input.GetKey("w"))
+        else if (Input.GetKey("b"))
         {
-            light.color = Color.white;
-			reveal.SetColor("_Color", Color.white);
+            lightSource.color = Color.blue;
+            reveal.SetTexture("_MainTex", blueTexture);
+            reveal.SetColor("_Color", Color.blue);
+        }
+        else if (Input.GetKey("y"))
+        {
+            lightSource.color = Color.green;
+            reveal.SetTexture("_MainTex", greenTexture);
+            reveal.SetColor("_Color", Color.green);
+        }
+        else if (Input.GetKey("w"))
+        {
+            lightSource.color = Color.white;
+            reveal.SetColor("_Color", Color.white);
         }
 
-        reveal.SetVector("_LightPosition", light.transform.position);
-        reveal.SetVector("_LightDirection", light.transform.forward);
-        reveal.SetFloat("_LightAngle", light.spotAngle);
+        reveal.SetVector("_LightPosition", lightSource.transform.position);
+        reveal.SetVector("_LightDirection", lightSource.transform.forward);
+        reveal.SetFloat("_LightAngle", lightSource.spotAngle);
 
     }
 }
